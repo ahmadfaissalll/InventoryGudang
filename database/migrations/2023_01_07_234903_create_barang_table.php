8ccf1a5f7f406636bbb71d02abe9d3da810e42fe
Ahmad Faisal <faisal718970@gmail.com>
2023-01-08 07:42:35 +0700

    Membuat Migration dan Model

---
+++ database/migrations/2023_01_07_234903_create_barang_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 50);
+            $table->string('jenis', 30);
+            $table->string('merk', 40);
+            $table->string('ukuran', 20);
+            $table->unsignedInteger('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+};
---
+++ database/migrations/2023_01_08_000339_create_barang_keluar_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('barang_keluar', function (Blueprint $table) {
+      $table->id();
+      $table->string('nama_barang', 55);
+      $table->foreignId('id_barang')->nullable()->constrained('barang')->nullOnDelete();
+      $table->unsignedInteger('jumlah');
+      $table->string('penerima', 100);
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('barang_keluar');
+  }
+};
